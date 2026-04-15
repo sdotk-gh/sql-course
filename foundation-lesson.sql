d9ccb6fb05ab4fd85995ae3cb8d259e66d9c382e
sdotk-gh <saqib.khan5@nhs.net>
2026-04-15 12:37:59 +0000

    statements

--- foundation-lesson.sql
+++ foundation-lesson.sql
@@ -9,7 +9,7 @@ Comments can also be across several lines
 SQL does not care about white space or capitalisation but you should!
 */
 -- simplest statement, bring back all data from a table
-SELECT 
+SELECT
 	*
 FROM
 	PatientStay;
@@ -18,12 +18,17 @@ FROM
 Rather than * to select all columns, choose the columns you want
 */
 SELECT
-	PatientId
-	, Tariff
-	, Ward
-	, Hospital
+	ps.PatientId
+		
+	,ps.Tariff
+			
+	,ps.Ward
+				,ps.AdmittedDate
+					
+	,ps.Hospital
+
 FROM
-	PatientStay;
+	PatientStay ps;
 
 /*
 Using a table alias (ps in the example below) is good practice and helps in a few ways 
@@ -32,9 +37,12 @@ Using a table alias (ps in the example below) is good practice and helps in a fe
 */
 SELECT
 	p.PatientId
-	, p.Ward
-	, p.Tariff
-	, p.Hospital
+	
+	,p.Ward
+	
+	,p.Tariff
+	
+	,p.Hospital
 FROM
 	PatientStay p;
 
@@ -44,12 +52,25 @@ Note: we can also AND and OR clauses
 */
 SELECT
 	ps.PatientId
-	, ps.AdmittedDate
-	, ps.Hospital
-	, ps.Ward
-	, ps.Tariff
+	,ps.AdmittedDate
+	,ps.DischargeDate
+	,DATEADD(month,3,ps.DischargeDate) AS AppointmentDate
+	,dateadd (week, -2, ps.AdmittedDate) AS ReminderDate
+	,DATEDIFF(DAY,ps.AdmittedDate,ps.DischargeDate) AS LOS
+	,ps.Hospital
+	,ps.Ward
+	,ps.Tariff
 FROM
-	PatientStay ps;
+	PatientStay ps
+
+WHERE
+	ps.Hospital IN ('Kingston','pruh')
+--AND
+--	ps.Ward like '%Surgery'
+--AND
+--	ps.AdmittedDate BETWEEN '2024-02-27' and '2024-03-01'
+ORDER BY ps.AdmittedDate ASC
+	, ps.Tariff DESC
 
 /*
 some alternative WHERE clauses.  Try these out
@@ -66,15 +87,19 @@ _ means any single character
 
 SELECT
 	ps.PatientId
-	, ps.AdmittedDate
-	, ps.Hospital
-	, ps.Ward
-	, ps.Tariff
+	
+	,ps.AdmittedDate
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,ps.Tariff
 FROM
 	PatientStay ps
 WHERE
 	ps.Hospital IN ('Kingston', 'PRUH');
-	--WHERE ps.Hospital LIKE 'Kin%'
+--WHERE ps.Hospital LIKE 'Kin%'
 
 /*
 Sort: by the values of one or more columns with the ORDER BY clause
@@ -84,10 +109,14 @@ Sorts smallest to largest (ASCending) by default
 -- ORDER BY a single column
 SELECT
 	ps.PatientId
-	, ps.AdmittedDate
-	, ps.Hospital
-	, ps.Ward
-	, ps.Tariff
+	
+	,ps.AdmittedDate
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,ps.Tariff
 FROM
 	PatientStay ps
 ORDER BY
@@ -96,10 +125,14 @@ ORDER BY
 -- ORDER BY several columns
 SELECT
 	ps.PatientId
-	, ps.AdmittedDate
-	, ps.Hospital
-	, ps.Ward
-	, ps.Tariff
+	
+	,ps.AdmittedDate
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,ps.Tariff
 FROM
 	PatientStay ps
 ORDER BY
@@ -112,12 +145,17 @@ Add a column with an expression in the column list and a column alias
 */
 SELECT
 	ps.PatientId
-	, ps.AdmittedDate
+	
+	,ps.AdmittedDate
 	-- See documentation for DATEADD at https://www.w3schools.com/sql/func_sqlserver_dateadd.asp
-	, DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
-	, ps.Hospital
-	, ps.Ward
-	, ps.Tariff
+	
+	,DATEADD(WEEK, -2, ps.AdmittedDate) AS ReminderDate
+	
+	,ps.Hospital
+	
+	,ps.Ward
+	
+	,ps.Tariff
 FROM
 	PatientStay ps;
 
@@ -131,19 +169,22 @@ Aggregate is to get a single result from a set of numbers
 Aggregation functions include SUM() and COUNT(*) but also MIN(), MAX(), AVERAGE()..
 We can group by at whatever level of aggregation we need and calculate several aggregations
 */
-	
+
 -- Aggregate over the entire dataset
 SELECT
 	COUNT(*) AS NumberOfPatients
-	, SUM(ps.Tariff) AS TotalTariff
+	
+	,SUM(ps.Tariff) AS TotalTariff
 FROM
 	PatientStay ps;
 
 -- GROUP BY a single column
 SELECT
 	ps.AdmittedDate
-	, COUNT(*) AS NumberOfPatients
-	, SUM(ps.Tariff) AS TotalTariff
+	
+	,COUNT(*) AS NumberOfPatients
+	
+	,SUM(ps.Tariff) AS TotalTariff
 FROM
 	PatientStay ps
 GROUP BY
@@ -152,9 +193,12 @@ GROUP BY
 -- GROUP BY two columns
 SELECT
 	ps.AdmittedDate
-	, ps.Hospital
-	, COUNT(*) AS NumberOfPatients
-	, SUM(ps.Tariff) AS TotalTariff
+	
+	,ps.Hospital
+	
+	,COUNT(*) AS NumberOfPatients
+	
+	,SUM(ps.Tariff) AS TotalTariff
 FROM
 	PatientStay ps
 GROUP BY
@@ -167,10 +211,14 @@ Remember that the WHERE clause filters the data before it is aggregated
 */
 SELECT
 	AdmittedDate
-	, SUM(ps.Tariff) AS TotalTariff
-	, MIN(ps.Tariff) AS SmallestTariff
-	, AVG(ps.Tariff) AS AverageTariff
-	, COUNT(*) AS NumberOfTariffs
+	
+	,SUM(ps.Tariff) AS TotalTariff
+	
+	,MIN(ps.Tariff) AS SmallestTariff
+	
+	,AVG(ps.Tariff) AS AverageTariff
+	
+	,COUNT(*) AS NumberOfTariffs
 FROM
 	PatientStay ps
 WHERE
@@ -193,7 +241,7 @@ SELECT
 	*
 FROM
 	PatientStay ps
-JOIN DimHospital h ON
+	JOIN DimHospital h ON
 	ps.Hospital = h.Hospital;
 
 /*
@@ -202,10 +250,13 @@ JOIN DimHospital h ON
 
 SELECT
 	ps.PatientId
-	, ps.AdmittedDate
-	, h.HospitalType
-	, h.HospitalSize
+	
+	,ps.AdmittedDate
+	
+	,h.HospitalType
+	
+	,h.HospitalSize
 FROM
 	PatientStay ps
-JOIN DimHospital h ON
+	JOIN DimHospital h ON
 	ps.Hospital = h.Hospital;
